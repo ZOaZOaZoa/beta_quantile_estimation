0360d70e7bc8550b118364ca426910155c5a8c35
ZOaZOaZoa <vbifbk555@gmail.com>
2024-06-03 17:03:23 +0300

    added description

--- table.py
+++ table.py
@@ -3,12 +3,17 @@ import pandas as pd
 from scipy.special import betaincinv
 from tqdm import tqdm
 
+first = 0.1
+last = 100
+total = 100
+probability = 0.95
+
 table = []
-a_list = b_list = np.linspace(0.1, 100, 250).tolist()
+a_list = b_list = np.linspace(first, last, total).tolist()
 for a in tqdm(a_list, leave=False):
     row = []
     for b in b_list:
-        row += [betaincinv(a, b, 0.95),]
+        row += [betaincinv(a, b, probability),]
     table += [row, ]
 
 table_df = pd.DataFrame(table, columns=b_list, index=a_list)
